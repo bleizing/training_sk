03c713cc8b4d743703990e491ed6eba4d2d67b40
bleizing <bleizing97@gmail.com>
2021-05-26 21:43:39 +0700

    Add email column to User

--- src/main/java/com/bleizing/sejuta_kebaikan_be/model/User.java
+++ src/main/java/com/bleizing/sejuta_kebaikan_be/model/User.java
@@ -15,6 +15,9 @@ import com.sun.istack.NotNull;
 public class User extends BaseModel {
 	@NotNull
 	private String name;
+	
+	@Column
+	private String email;
 
 	public String getName() {
 		return name;
@@ -22,5 +25,13 @@ public class User extends BaseModel {
 
 	public void setName(String name) {
 		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
 	}
 }
